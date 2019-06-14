2918621e70861e04a7570c056ba5796fffaa9a0c
amuhhabibi <46309781+amuhhabibi@users.noreply.github.com>
2019-06-15 02:25:42 +0800

    Add files via upload

--- family100/script.js
+++ family100/script.js
@@ -1,57 +1,114 @@
-var x = 1;
+var x = 0; //silang
+var y = 1; //soal
+var op1 = document.getElementById('op1');
+var op2 = document.getElementById('op2');
+var op3 = document.getElementById('op3');
+var op4 = document.getElementById('op4');
+var op5 = document.getElementById('op5');
+var op6 = document.getElementById('op6');
+var silang1 = document.getElementById('silang1');
+var silang2 = document.getElementById('silang2');
+var silang3 = document.getElementById('silang3');
+
 
 function tambah_silang(){
-
-	if (x == 1) {
-	    document.getElementById('silang1').setAttribute("class", "silang_on");
-	    ++x;
-	}else if (x == 2) {
-	    document.getElementById('silang1').setAttribute("class", "silang_on");
-		document.getElementById('silang2').setAttribute("class", "silang_on");
+	if (x <= -1){
 		++x;
+	}else if (x >= 4){
+		--x;
+	}else if (x == 0) {
+	    silang1.removeAttribute("class", "silang_on");
+		silang2.removeAttribute("class", "silang_on");
+		silang3.removeAttribute("class", "silang_on");
+	}else if (x == 1) {
+	    silang1.setAttribute("class", "silang_on");
+		silang2.removeAttribute("class", "silang_on");
+		silang3.removeAttribute("class", "silang_on");
+	}else if (x == 2) {
+	    silang1.setAttribute("class", "silang_on");
+		silang2.setAttribute("class", "silang_on");
+		silang3.removeAttribute("class", "silang_on");
 	}else if (x == 3) {
-	    document.getElementById('silang1').setAttribute("class", "silang_on");
-		document.getElementById('silang2').setAttribute("class", "silang_on");
-		document.getElementById('silang3').setAttribute("class", "silang_on");
+	    silang1.setAttribute("class", "silang_on");
+		silang2.setAttribute("class", "silang_on");
+		silang3.setAttribute("class", "silang_on");
+	}
+}
+
+function pertanyaan(){
+	if (y <= 0){
+		++y;
+	}else if (y >= 3){
+		--y;
+	}else if (y == 1) {
+		document.getElementById('tema').innerHTML = "Bagian tubuh yang bergerak saat tertawa :";
+		op1.innerHTML = "(43) Mulut";
+		op2.innerHTML = "(18) Pipi";
+		op3.innerHTML = "(13) Perut";
+		op4.innerHTML = "(9) Bahu";
+		op5.innerHTML = "(4) Dada";
+		op6.innerHTML = "(3) Dagu";
+	}else if (y == 2) {
+		document.getElementById('tema').innerHTML = "Jenis binatang berbisa :";
+		op1.innerHTML = "(36) Ular";
+		op2.innerHTML = "(21) Kelabang";
+		op3.innerHTML = "(17) Laba-laba";
+		op4.innerHTML = "(14) Tawon";
+		op5.innerHTML = "(8) Lebah";
+		op6.innerHTML = "(4) Kalajengking";
 	}
 }
 
 window.addEventListener("keydown", checkKeyPress, false);
 function checkKeyPress(key){
 	if (key.keyCode == "49") {
-		document.getElementById('op1').setAttribute("class", "list_on");
+		document.getElementById("op1").classList.toggle("list_on");
 	}else if (key.keyCode == "50") {
-		document.getElementById('op2').setAttribute("class", "list_on");
+		op2.classList.toggle("list_on");
 	}else if (key.keyCode == "51") {
-		document.getElementById('op3').setAttribute("class", "list_on");
+		op3.classList.toggle("list_on");
 	}else if (key.keyCode == "52") {
-		document.getElementById('op4').setAttribute("class", "list_on");
+		op4.classList.toggle("list_on");
 	}else if (key.keyCode == "53") {
-		document.getElementById('op5').setAttribute("class", "list_on");
+		op5.classList.toggle("list_on");
 	}else if (key.keyCode == "54") {
-		document.getElementById('op6').setAttribute("class", "list_on");
-	}else if (key.keyCode == "88") {
+		op6.classList.toggle("list_on");
+	}else if (key.keyCode == "83") {
+		++y;
+		x = 0;
+	    silang1.removeAttribute("class", "silang_on");
+		silang2.removeAttribute("class", "silang_on");
+		silang3.removeAttribute("class", "silang_on");
+	    op1.removeAttribute("class", "list_on");
+		op2.removeAttribute("class", "list_on");
+		op3.removeAttribute("class", "list_on");
+	    op4.removeAttribute("class", "list_on");
+		op5.removeAttribute("class", "list_on");
+		op6.removeAttribute("class", "list_on");
+		pertanyaan();
 		tambah_silang();
+	}else if (key.keyCode == "65") {
+		--y;
+		x = 0;
+	    op1.removeAttribute("class", "list_on");
+		op2.removeAttribute("class", "list_on");
+		op3.removeAttribute("class", "list_on");
+	    op4.removeAttribute("class", "list_on");
+		op5.removeAttribute("class", "list_on");
+		op6.removeAttribute("class", "list_on");
+		pertanyaan();
+		tambah_silang()
+	}else if (key.keyCode == "88") {
+		++x;
+		tambah_silang();
+	}else if(key.keyCode == "90"){
+		--x;
+		tambah_silang()
+	}else if(key.keyCode == "27"){
+		modal.style.display = "none";
+	}else if(key.keyCode == "72"){
+		modal.style.display = "block";
 	}
-}
-
-function turnon1(){
-	document.getElementById('op1').setAttribute("class", "list_on");
-}
-function turnon2(){
-	document.getElementById('op2').setAttribute("class", "list_on");
-}
-function turnon3(){
-	document.getElementById('op3').setAttribute("class", "list_on");
-}
-function turnon4(){
-	document.getElementById('op4').setAttribute("class", "list_on");
-}
-function turnon5(){
-	document.getElementById('op5').setAttribute("class", "list_on");
-}
-function turnon6(){
-	document.getElementById('op6').setAttribute("class", "list_on");
 }
 
 var modal = document.getElementById("myModal");
@@ -69,3 +126,5 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
