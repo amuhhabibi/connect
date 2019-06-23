dc8cd9c19af947c6f32e9f5e59a4f9f273153418
amuhhabibi-mac-desktop <46309781+amuhhabibi@users.noreply.github.com>
2019-06-23 16:11:34 +0800

    changed index.html

---
+++ pwMech.js
@@ -0,0 +1,13 @@
+function pwCheck(){
+	var password = document.getElementById("password").value;
+	location.replace("/" + password);
+}
+
+var input = document.getElementById("password");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    pwCheck();
+    event.preventDefault();
+    pwCheck();
+  }
+});
