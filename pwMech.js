c9301f5ac10ec4b385640fe43ba2d31fa6470fa7
amuhhabibi-mac-desktop <46309781+amuhhabibi@users.noreply.github.com>
2019-09-16 22:23:09 +0800

    oppopodadte

--- pwMech.js
+++ pwMech.js
@@ -1,23 +1,23 @@
-function pwCheck(){
-    event.preventDefault();
-	var password = document.getElementById("password").value;
-	window.location = "/" + password;
-}
+// function pwCheck(){
+//     event.preventDefault();
+// 	var password = document.getElementById("password").value;
+// 	window.location = "/" + password;
+// }
 
-function pwCheckBtn(){
-	var x = event.keyCode;
-	if (x == 13) {
-	  	pwCheck();
-	}
-}
+// function pwCheckBtn(){
+// 	var x = event.keyCode;
+// 	if (x == 13) {
+// 	  	pwCheck();
+// 	}
+// }
 
 // function valueCheck(){
 // 	var x = document.getElementById("list").value;
 // 	window.location = "/" + x;
 // }
 
-function mGallery(){
-	window.location = "/gallery";
+function mProfile(){
+	window.location = "/profile/";
 }
 
 // function mMabaca(){
