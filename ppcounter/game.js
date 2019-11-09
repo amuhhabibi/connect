39a70ebc440310418d32f278dc1dd79722ef327b
amuhhabibi-mac-desktop <46309781+amuhhabibi@users.noreply.github.com>
2019-11-09 21:54:01 +0800

    mamaan

--- ppcounter/game.js
+++ ppcounter/game.js
@@ -1,6 +1,8 @@
 let user1Score = 0;
 let user2Score = 0;
 let turnCounter = 0;
+const user1Name = document.getElementById('user1-label');
+const user2Name = document.getElementById('user2-label');
 const turnDesc = document.getElementById('userNamesTurn');
 const user1Score_span = document.getElementById('user1-score');
 const user2Score_span = document.getElementById('user2-score');
@@ -11,27 +13,50 @@ function user1AddPoint() {
     user1Score++;
     user1Score_span.innerHTML = user1Score;
     turnCheck();
+    setName()
 }
 
 function user2AddPoint() {
     user2Score++;
     user2Score_span.innerHTML = user2Score;
     turnCheck();
-}
-
-function isOdd(n) {
-    return n % 2 == 1;
+    setName()
 }
 
 function turnCheck() {
     const z = (user1Score + user2Score) % 4;
     if (z == 2) {
-        turnDesc.innerHTML = "User2";
+        turnDesc.innerHTML = user2Name.value;
     } else if (z == 1) {
-        turnDesc.innerHTML = "User1";
+        turnDesc.innerHTML = user1Name.value;
     } else if (z == 0) {
-        turnDesc.innerHTML = "User1"
+        turnDesc.innerHTML = user1Name.value;
     } else if (z == 3) {
-        turnDesc.innerHTML = "User2"
+        turnDesc.innerHTML = user2Name.value;
     }
 }
+
+function loadName() {
+    if (localStorage.user1Name_local) {
+        user1Name.value = localStorage.user1Name_local;
+    } else {
+        localStorage.user1Name_local = 'p1';
+    }
+    if (localStorage.user2Name_local) {
+        user2Name.value = localStorage.user2Name_local;
+    } else {
+        localStorage.user2Name_local = 'p2';
+    }
+}
+
+function setName() {
+    localStorage.user1Name_local = user1Name.value;
+    localStorage.user2Name_local = user2Name.value;
+}
+
+function setNameOnLoad() {
+    turnDesc.innerHTML = user1Name.value;
+}
+
+loadName();
+setNameOnLoad();
