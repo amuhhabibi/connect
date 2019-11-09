271f539173129e0ffdf7864b35d449ff55c4cf22
amuhhabibi-mac-desktop <46309781+amuhhabibi@users.noreply.github.com>
2019-11-09 08:19:56 +0800

    beautified

--- pwMech.js
+++ pwMech.js
@@ -1,23 +1,23 @@
-function pwCheck(){
+function pwCheck() {
     event.preventDefault();
-	var password = document.getElementById("password").value;
-	window.location = "/" + password;
+    var password = document.getElementById("password").value;
+    window.location = "/" + password;
 }
 
-function pwCheckBtn(){
-	var x = event.keyCode;
-	if (x == 13) {
-	  	pwCheck();
-	}
+function pwCheckBtn() {
+    var x = event.keyCode;
+    if (x == 13) {
+        pwCheck();
+    }
 }
 
-function valueCheck(){
-	var x = document.getElementById("list").value;
-	window.location = "/" + x;
+function valueCheck() {
+    var x = document.getElementById("list").value;
+    window.location = "/" + x;
 }
 
-function mProfile(){
-	window.location = "/rumah/";
+function mProfile() {
+    window.location = "/rumah/";
 }
 
 // function mMabaca(){
