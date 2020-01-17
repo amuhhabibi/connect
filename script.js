19b4ba83e305827e41095b1800b0edabc7721958
amuhhabibi-mac-desktop <46309781+amuhhabibi@users.noreply.github.com>
2020-01-18 07:35:44 +0800

    18/01/07

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var txt = [];
 var article = [];
 
 function preload() {
-  for (i = 1; i < 4; i++) {
+  for (i = 1; i < 100; i++) {
     article.push(loadStrings("mabaca/article/article00" + i + ".txt"));
   }
 }
@@ -18,21 +18,7 @@ function setup() {
     txt[i] = txt[i].split(" ").slice(0, 35);
     txt[i] = txt[i].join(" ");
     document.getElementById("title" + (i + 1)).innerText = title[i];
-    document.getElementById("text" + (i + 1)).innerText = txt[i] + ". . .";
+    document.getElementById("text" + (i + 1)).innerText = txt[i] + " . . . ";
   }
   remove();
-  console.log(article);
 }
-
-// function setup() {
-//   txt = article.slice(1, 3);
-//   txt = txt.filter(v => v != "");
-//   txt = txt + " ";
-//   txt = txt.split(" ").slice(0, 35);
-//   txt = txt.join(" ");
-//   txt = txt + " . . .";
-//   title = article.slice(0, 1);
-//   document.getElementById("text1").innerText = txt;
-//   document.getElementById("title1").innerText = title;
-//   remove();
-// }
