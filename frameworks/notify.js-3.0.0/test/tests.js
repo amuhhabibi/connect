080e2585e1485a30dd03a8b80b0391e05e84578c
amuhhabibi-mac-desktop <46309781+amuhhabibi@users.noreply.github.com>
2020-02-22 07:45:03 +0800

    notif test

---
+++ frameworks/notify.js-3.0.0/test/tests.js
@@ -0,0 +1,184 @@
+/* global Notify, describe, beforeEach, afterEach, it, expect, sinon, spyOn, jasmine */
+
+describe('instantiation', function() {
+
+    'use strict';
+
+    it('should create a new Notify instance', function() {
+        var notification = new Notify('foo');
+        expect(notification instanceof window.Notify).toBeTruthy();
+    });
+
+    it('should throw an exception if has no title', function() {
+        expect(function() {
+            new Notify();
+        }).toThrow();
+    });
+});
+
+describe('isSupported', function() {
+
+    'use strict';
+
+    it('should return true when notifications are supported', function() {
+        expect(Notify.isSupported()).toBeTruthy();
+    });
+
+    it('should throw an error if permission has already been granted', function() {
+        expect(function() {
+            Notify.isSupported('granted');
+        }).toThrow();
+    });
+});
+
+describe('permission', function() {
+
+    'use strict';
+
+    afterEach(function() {
+        Notify.permissionLevel = Notification.permission;
+    });
+
+    it('should check if permission is needed', function() {
+        expect(typeof Notify.needsPermission).toBe('boolean');
+    });
+
+    it('should request permission from the user', function() {
+        spyOn(window.Notification, 'requestPermission');
+        Notify.requestPermission();
+        expect(window.Notification.requestPermission).toHaveBeenCalled();
+    });
+
+    describe('requestPermission (granted)', function() {
+        beforeEach(function(done) {
+            spyOn(window.Notification, 'requestPermission').and.callFake(function(cb) {
+                cb('granted');
+                done();
+            });
+            Notify.requestPermission();
+        });
+
+        it('should update permissionLevel and needsPermission if the user accepts the request', function() {
+            expect(Notify.needsPermission).toBeFalsy();
+        });
+    });
+
+    describe('requestPermission (denied)', function() {
+        beforeEach(function(done) {
+            spyOn(window.Notification, 'requestPermission').and.callFake(function(cb) {
+                cb('denied');
+                done();
+            });
+            Notify.requestPermission();
+        });
+
+        it('should update permissionLevel and needsPermission if the user rejects the request', function() {
+            expect(Notify.needsPermission).toBeTruthy();
+        });
+    });
+});
+
+describe('callbacks', function() {
+
+    'use strict';
+
+    var callback;
+
+    beforeEach(function() {
+        callback = jasmine.createSpy();
+    });
+
+    it('should fire show callback', function() {
+        var notification = new Notify('foo', {
+            notifyShow: callback
+        });
+        notification.onShowNotification();
+        expect(callback).toHaveBeenCalled();
+    });
+
+    it('should fire close callback', function() {
+        var notification = new Notify('foo', {
+            notifyClose: callback
+        });
+        notification.show();
+        notification.onCloseNotification();
+        expect(callback).toHaveBeenCalled();
+    });
+
+    it('should fire click callback', function () {
+        var notification = new Notify('foo', {
+            notifyClick: callback
+        });
+        notification.onClickNotification();
+        expect(callback).toHaveBeenCalled();
+    });
+
+    it('should close if closeOnClick is set when fire click callback', function () {
+        var notification = new Notify('foo', {
+            notifyClick: callback,
+            requireInteraction: true,
+            closeOnClick: true
+        });
+        spyOn(notification, 'close');
+        notification.onClickNotification();
+        expect(callback).toHaveBeenCalled();
+        expect(notification.close).toHaveBeenCalled();
+    });
+
+    it('should fire error callback', function () {
+        var notification = new Notify('foo', {
+            notifyError: callback
+        });
+        notification.show();
+        notification.onErrorNotification();
+        expect(callback).toHaveBeenCalled();
+    });
+
+    it('should destroy a notification once closed', function() {
+        var notification = new Notify('foo', {
+            notifyClose: callback
+        });
+        spyOn(notification, 'destroy');
+        notification.onCloseNotification();
+        expect(notification.destroy).toHaveBeenCalled();
+
+    });
+});
+
+describe('timeout', function() {
+
+    'use strict';
+
+    var clock;
+
+    beforeEach(function() {
+        clock = sinon.useFakeTimers();
+    });
+
+    afterEach(function() {
+        clock.restore();
+    });
+
+    it('should close a notification automatically', function() {
+        var notification = new Notify('foo', {
+            timeout: 1
+        });
+        spyOn(window.Notification.prototype, 'close');
+        notification.show();
+        expect(window.Notification.prototype.close).not.toHaveBeenCalled();
+        clock.tick(1000);
+        expect(window.Notification.prototype.close).toHaveBeenCalled();
+    });
+
+    it('should not close a notification automatically with requireInteraction', function () {
+        var notification = new Notify('foo', {
+            timeout: 1,
+            requireInteraction: true
+        });
+        spyOn(window.Notification.prototype, 'close');
+        notification.show();
+        expect(window.Notification.prototype.close).not.toHaveBeenCalled();
+        clock.tick(1000);
+        expect(window.Notification.prototype.close).not.toHaveBeenCalled();
+    });
+});
