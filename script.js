67319d4f373c0f9b9550bb8a0f3ab7733389da61
amuhhabibi-mac-desktop <46309781+amuhhabibi@users.noreply.github.com>
2020-02-25 10:15:06 +0800

    mmomomo

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ var txt = [];
 var article = [];
 
 function preload() {
-  for (i = 1; i < 4; i++) {
-    v = i.toString().padStart(3, "0");
-    article.push(loadStrings("mabaca/article/article" + v + ".txt"));
-  }
+    for (i = 1; i < 4; i++) {
+        v = i.toString().padStart(3, "0");
+        article.push(loadStrings("mabaca/article/article" + v + ".txt"));
+    }
 }
 
 function setup() {
-  for (i = 0; i < 3; i++) {
-    article[i] = article[i].filter(v => v != "");
-    title[i] = article[i].slice(0, 1);
-    txt[i] = article[i].slice(1, article.length - 1);
-    txt[i] = txt[i] + " ";
-    txt[i] = txt[i].split(" ").slice(0, 35);
-    txt[i] = txt[i].join(" ");
-    document.getElementById("title" + (i + 1)).innerText = title[i];
-    document.getElementById("text" + (i + 1)).innerText = txt[i] + " . . . ";
-  }
-  remove();
+    for (i = 0; i < 3; i++) {
+        article[i] = article[i].filter(v => v != "");
+        title[i] = article[i].slice(0, 1);
+        txt[i] = article[i].slice(1, article.length - 1);
+        txt[i] = txt[i] + " ";
+        txt[i] = txt[i].split(" ").slice(0, 35);
+        txt[i] = txt[i].join(" ");
+        document.getElementById("title" + (i + 1)).innerText = title[i];
+        document.getElementById("text" + (i + 1)).innerText = txt[i] + " . . . ";
+    }
+    remove();
 }
