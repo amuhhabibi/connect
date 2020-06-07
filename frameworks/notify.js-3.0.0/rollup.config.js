6d0fdf507f06d62a57f6d4c857e50a0328fd6b52
Habibi <bibie@Habibis-MacBook-Air.local>
2020-06-08 07:38:34 +0800

    k

--- frameworks/notify.js-3.0.0/rollup.config.js
+++
@@ -1,9 +0,0 @@
-import babel from 'rollup-plugin-babel';
-
-export default {
-    entry: 'src/notify.js',
-    dest: 'dist/notify.js',
-    format: 'umd',
-    moduleName: 'Notify',
-    plugins: [ babel() ]
-};
