b8c824e229b41ba49fe08da6a2bfb8dcbe7d543c
amuhhabibi <46309781+amuhhabibi@users.noreply.github.com>
2021-12-31 21:31:08 +0700

    happynewyear

--- countdown/script.js
+++ countdown/script.js
@@ -3,7 +3,7 @@ const setTheme = theme => document.documentElement.className = theme;
 let y = document.documentElement.className;
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2022 00:00:00").getTime();
+var countDownDate = new Date("Jan 1, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
